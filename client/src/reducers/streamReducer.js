d9811015dd4ed32926d24b18469fb4e85fa34ddb
fortune <omorodion.fortune@gmail.com>
2022-03-06 00:47:24 +0100

    create helper method for stream edit and delete

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,23 +1,23 @@
 import _ from "lodash";
 import {
-  fetchStreams,
-  fetchStream,
-  createStream,
-  editStream,
-  deleteStream,
-} from "../actions";
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
-    case fetchStreams:
+    case FETCH_STREAM:
       return { ...state, ..._.mapKeys(action.payload, "id") };
-    case fetchStream:
+    case FETCH_STREAMS:
       return { ...state, [action.payload.id]: action.payload };
-    case createStream:
+    case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
-    case editStream:
+    case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
-    case deleteStream:
+    case DELETE_STREAM:
       return _.omit(state, action.payload);
     default:
       return state;
